2fafc1374ee4af06533b9738b71f89f2aacae534
BernardoManfriani <bernardomanfriani@gmail.com>
2020-02-03 12:46:53 +0100

    Delete socketManager.h

--- socketManager.h
+++
@@ -1,6 +0,0 @@
-// look: ifndef DA METTERE ?
-#define DEFAULT_PROTOCOL 0
-
-extern int readSocket(int fd, char *str);
-extern void writeSocket(int socketFd, char *data);
-extern int connectClient(char *socketName);
